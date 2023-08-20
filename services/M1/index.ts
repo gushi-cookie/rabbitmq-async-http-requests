95cdcc259a4c019b1158086252645e27807761c6
gushi-cookie <926396z@mail.ru>
2023-08-20 11:59:21 +0300

    Add "morgan" library to M1 service. Create logs.

--- services/M1/index.ts
+++ services/M1/index.ts
@@ -1,19 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
 import rpcTasksRouter from './src/routes/rpcTasks.js';
 import { RabbitConnection, establishRabbitConn } from './src/utils/amqp.util.js';
 import handleErrors from './src/middlewares/handleErrors.js';
 
 
-const originalLogFn = console.log;
-console.log = function(message: any) {
-    originalLogFn(`[M1: Backend] ${message}`);
-};
-
 
 const expressPort = process.env.PORT;
 const rabbitPort = Number(process.env.RABBIT_PORT);
 
+console.log('Setting up the express app.');
 const app = express();
+app.use(morgan('common'));
 app.use(rpcTasksRouter);
 app.use(handleErrors);
 
